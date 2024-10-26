d8451c51976f14bacec8e3a5989f60056cd9e764
Irshad Nizami <irniz@rediffmail.com>
2024-10-26 23:40:31 +0530

    Parameterize image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t irschad/java-app:2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push irschad/java-app:2.0'
+        sh "docker push $imageName"
     }
 }
